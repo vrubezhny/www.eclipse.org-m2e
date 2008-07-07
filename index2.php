e408a734b0239455a26987c9f80cd7ec506fa4f9
ekuleshov <ekuleshov>
2008-07-07 20:33:09 +0000

    analytics support

--- index2.php
+++ index2.php
@@ -71,10 +71,20 @@
 	</div>
 
   <div id="rightcolumn">
-        $sidebar
+    $sidebar
   </div>
 	
-EOHTML;
+  <script type="text/javascript">
+    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+  </script>
+  <script type="text/javascript">
+    var pageTracker = _gat._getTracker("UA-1693297-7");
+    pageTracker._initData();
+    pageTracker._trackPageview();
+  </script>
+  
+  EOHTML;
 
 	# Generate the web page
 	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
