d0d5cb6bc3fa849f21dc6983ff54c15ce8e32036
wisberg <wisberg>
2008-07-14 06:09:39 +0000

    draft goal index (to remove on decision)

--- index3.php
+++ index3.php
@@ -21,32 +21,31 @@
 	$html = <<<EOHTML
    
 	<div id="midcolumn">
-	<h1>Maven Eclipse</h1>
-	<h2>Good news! m2eclipse is moving to eclipse.org!</h2>
-    <p>After years at <a href="http://m2eclipse.codehaus.org/">Codehaus</a> (thanks!), 
-    we're getting closer to the eclipse.org projects we work with, 
+	<h1>m2eclipse is moving to eclipse.org!</h1>
+    <p>After years at <a href="http://m2eclipse.codehaus.org/">Codehaus</a>, 
+    we're moving to get closer to the eclipse.org projects we work with, 
     like PDE, WTP, and AJDT.
-    We are staying at 
-    <a href="http://m2eclipse.codehaus.org/">http://m2eclipse.codehaus.org/</a>
-    for one more release, and will move when we have everything set up so
-    everyone can do everything right here.</p>
-    <p>We will soon be caught in details like feature lists, so now is a good time    
+    We are staying at Codehaus for one more release, but we'll ask everyone
+    to come over once we have everything set up here.</p>
+    
+    <p>Before we get caught up in details, now is a good time    
     to step back and talk about why we think bringing Maven to Eclipse could be very fruitful.  
-    If you see more benefits or potential risks, let's talk about it 
-    (see <a href="#contacts">below</a>).
+    If you see more benefits or potential risks or if you'd like to participate, 
+    let's talk about it (see <a href="#contacts">below</a>).
     </p>
 	
 	<h2>Why Maven and Eclipse?</h2>
 	<p>Maven helps Eclipse build, and Eclipse helps people use Maven.
-	That makes it much easier for the Eclipse IDE to build more kinds of software projects,
-	and easier for more Eclipse projects to be built on the command-line or in 
-	a process of continuous integration.
+	With Maven, Eclipse will build more kinds of software projects both in the IDE
+	and on the command-line, e.g., in a process of continuous integration.
 	</p>
     <p>The <a href="http://maven.apache.org/">Apache Maven project</a> describes
     Maven as a "software project management and comprehension tool".  Maven is used in 
     thousands of open-source and commercial software projects to simplify building
     and understanding software of all stripes.  Maven is great at doing command-line 
-    builds, producing reports and documentation, and sharing code in binary repositories.
+    builds, understanding project structure and relationships, 
+    and sharing code in binary repositories.  It's easy to write and configure a
+    Maven plug-in for a given build task, and hundreds of plug-ins are available.
     </p>
     
     <p>The Eclipse IDE is great at supporting interactive software development, with 
@@ -57,15 +56,18 @@
     by Maven (because it is a much harder problem to write good UI support).
     </p>
     
-    <p>So Eclipse can make working with Maven easier, and Maven can make it supporting
-    many kinds of software in Eclipse easier.   Eclipse makes it easier to 
-    write Maven metadata and work with Maven dependencies.  Maven makes it easier
-    to manage library dependencies, to assemble projects, and to build from the command-line 
-    in a process of continuous integration.  All this helps both developers writing software and
+    <p>They can help each other out.
+    Eclipse can make it easier to work with Maven, and Maven can make it easier to     
+    support complex projects using different programming models.  
+    Eclipse editors make it easy to find and maintain project information, and
+    the Eclipse Java tools can use the Maven dependencies like any others for compiling,
+    searching, content assist, etc. (even in derived project types like WTP and AJDT).
+    Maven conventions and software repositories make it easy to manage library 
+    dependencies, to assemble projects, and to build from the command-line in a 
+    process of continuous integration.  All this helps both developers writing software and
     plug-in projects implementing software build or assembly processes.
     </p>
-    <p>Hence, we have three offerings, 
-    for software developers, Eclipse plug-in developers, and software build engineers:</p>
+    <p>Hence, we have something for everyone:</p>
      <ol>
      <li><em>Maven Eclipse Tools</em>, the UI for developers using Maven to make software</li>
      <li><em>Maven Eclipse Frameworks</em>, the API's for Eclipse plug-in's using Maven to 
@@ -86,7 +88,7 @@ and then email
 </li>
 <li>
 
-    <form method="post" action="http://www.sonatype.com/m2eclipse/announce">If you want us to let you know when we've moved, shoot us your email address:
+    <form method="post" action="http://www.sonatype.com/m2eclipse/announce">If you want us to let you know when we've moved, let us know your email address:
     <br><input name="email" maxlength="150" size="25" type="text"> 
 	<font size="-1">(This is just between us.)</font>
 </form>
@@ -95,6 +97,7 @@ and then email
    <p>
    The m2eclipse team
    </p>
+
     <hr class="clearer" />
     
 	</div>
