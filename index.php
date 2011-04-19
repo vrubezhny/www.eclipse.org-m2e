2d556784d1c7772190977b5d7bebf940f8665769
mpiggott <mpiggott>
2011-04-19 02:34:47 +0000

    [m2e] Remove sidebar from main page

--- index.php
+++ index.php
@@ -50,5 +50,5 @@
 EOHTML;
 
 	# Generate the web page
-	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+	$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
