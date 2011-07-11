f6acf34f9795fdbfd0d931ea65777745702fff75
ifedorenk <ifedorenk>
2011-07-11 19:48:55 +0000

    Updated to point at release repository

--- download/index.php
+++ download/index.php
@@ -6,9 +6,13 @@
 	<h3>Installing m2eclipse</h3>
 	<p>All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation Software User Agreement</a> unless otherwise specified.</p>
 	<p>m2e is tested against Eclipse 3.6 (Helios) and 3.7 (Indigo).</p>
+	<p>
+	See <a href="http://wiki.eclipse.org/M2E_updatesite_and_gittags">http://wiki.eclipse.org/M2E_updatesite_and_gittags</a> for detailed information about
+	available builds and m2e build repository layout.
+	</p>
 	<h3>Update Sites</h3>
-	<h4>m2e 1.0.x milestone build repository</h4>
-	<a href="http://download.eclipse.org/technology/m2e/milestones/1.0">http://download.eclipse.org/technology/m2e/milestones/1.0</a>
+	<h4>m2e release build repository</h4>
+	<a href="http://download.eclipse.org/technology/m2e/releases">http://download.eclipse.org/technology/m2e/releases</a>
 </div>
 EOHTML;
 	
