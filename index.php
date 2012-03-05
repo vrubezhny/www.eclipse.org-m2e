93d0894b7587dae1439027855b09c8093a786a49
ifedorenk <ifedorenk>
2012-03-05 15:55:36 +0000

    By popular demand, remove m2e video off the hopepage.

--- index.php
+++ index.php
@@ -31,8 +31,6 @@
 	<p>The goal of the m2ec project is to provide a first-class <a href="http://maven.apache.org">Apache Maven</a> support in the Eclipse IDE, making it easier to edit Maven's pom.xml, run a build from the IDE and much more. For Java developers, the very tight integration with  JDT greatly simplifies the consumption of  Java artifacts either being hosted on open source repositories such as <a href="http://mavencentral.sonatype.com">Maven Central</a>, or in your in-house Maven repository.<p/>
 	<p>m2e is also a platform that let others provide better integration with additional Maven plugins (e.g. Android, web development, etc.), and facilitates the distribution of those extensions through the m2e marketplace.<p/>
 	
-	<h3>m2e in minutes</h3>
-	<iframe class="youtube-player" type="text/html" width="640" height="385" src="http://www.youtube.com/embed/LyuMMqlpOq0" frameborder="0"></iframe>
 </div>
 
 <div id="rightcolumn">
