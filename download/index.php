7a3392298ab94d47f25d49bdc8a12d238400b7d4
ifedorenk <ifedorenk>
2012-06-27 13:26:44 +0000

    Updated with 1.1 and 1.2 info

--- download/index.php
+++ download/index.php
@@ -5,7 +5,7 @@
 <div id="midcolumn">
 	<h3>Installing m2eclipse</h3>
 	<p>All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation Software User Agreement</a> unless otherwise specified.</p>
-	<p>m2e is tested against Eclipse 3.6 (Helios) and 3.7 (Indigo).</p>
+	<p>m2e is tested against Eclipse 3.7 (Indigo) and 3.8/4.2 (Juno).</p>
 	<p>
 	See <a href="http://wiki.eclipse.org/M2E_updatesite_and_gittags">http://wiki.eclipse.org/M2E_updatesite_and_gittags</a> for detailed information about
 	available builds and m2e build repository layout.
@@ -19,11 +19,11 @@
 	<h4>Latest m2e release (recommended)</h4>
 	http://download.eclipse.org/technology/m2e/releases
 
-	<h4>m2e milestone builds towards version 1.1</h4>
-	http://download.eclipse.org/technology/m2e/milestones/1.1
+	<h4>m2e milestone builds towards version 1.2</h4>
+	http://download.eclipse.org/technology/m2e/milestones/1.2
 	
-	<h4>Latest m2e 1.1 SNAPSHOT build (not tested, not hosted at eclipse.org)</h4>
-	https://repository.sonatype.org/content/repositories/forge-sites/m2e/1.1.0/N/LATEST/
+	<h4>Latest m2e 1.2 SNAPSHOT build (not tested, not hosted at eclipse.org)</h4>
+	https://repository.sonatype.org/content/repositories/forge-sites/m2e/1.2.0/N/LATEST/
 </div>
 EOHTML;
 	
